f53f393067f1d6bd64f69afaaacab24a530454a2
fernandominguez <dominguez.fernando@gmail.com>
2020-11-05 15:36:43 +0100

    creating base class

--- gpeople.js
+++ gpeople.js
@@ -1,44 +1,14 @@
-// main
 const http = require("http");
 const https = require("https");
 
 /**
  * @tutorial https://developers.google.com/people/api/rest/?apix=true
  */
-const options = {
-  host: "people.googleapis.com",
-  port: 443,
-  path: "/v1/contactGroups:batchGet",
-  method: "GET",
-  encoding: "utf8",
-  headers: {
-    "Content-Type": "application/json"
-  }
+let GooglePeople = () => {
+  const options = {
+    host: "people.googleapis.com",
+    port: 443
+  };
 };
 
-function batchGetGooglePeople(options, result) {
-  const protocol = options.port == 443 ? https : http;
-
-  let output = "";
-
-  const request = protocol.request(options, (response) => {
-    response.setEncoding(options.encoding);
-
-    response.on("data", (chunk) => {
-      output += chunk;
-    });
-
-    response.on("end", () => {
-      let object = JSON.parse(output);
-      result(response.statusCode, object);
-    });
-  });
-
-  request.on("error", (error) => {
-    console.error(error);
-  });
-
-  request.end();
-}
-
-module.exports = batchGetGooglePeople;
+module.exports = GooglePeople;
