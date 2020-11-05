a50e5fb88eaf21aa0028c3e53b25eb9558baf8fe
fernandominguez <dominguez.fernando@gmail.com>
2020-11-05 15:38:26 +0100

    adding private Request functions

--- gpeople.js
+++ gpeople.js
@@ -7,8 +7,14 @@ const https = require("https");
 let GooglePeople = () => {
   const options = {
     host: "people.googleapis.com",
-    port: 443
+    port: 443,
   };
+
+  function _getRequest(options, result) {}
+  function _postRequest(options, result) {}
+  function _putRequest(options, result) {}
+  function _deleteRequest(options, result) {}
+  function _patchRequest(options, result) {}
 };
 
 module.exports = GooglePeople;
