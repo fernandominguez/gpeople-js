27910a40c06debbd12a520020375099d22d85817
fernandominguez <dominguez.fernando@gmail.com>
2020-11-05 11:34:04 +0100

    call for the Google People API batchGet method

--- gpeople.js
+++ gpeople.js
@@ -1,3 +1,44 @@
 // main
+const http = require("http");
+const https = require("https");
 
-module.exports = {};
+/**
+ * @tutorial https://developers.google.com/people/api/rest/?apix=true
+ */
+const options = {
+  host: "people.googleapis.com",
+  port: 443,
+  path: "/v1/contactGroups:batchGet",
+  method: "GET",
+  encoding: "utf8",
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+
+function batchGetGooglePeople(options, result) {
+  const protocol = options.port == 443 ? https : http;
+
+  let output = "";
+
+  const request = protocol.request(options, (response) => {
+    response.setEncoding(options.encoding);
+
+    response.on("data", (chunk) => {
+      output += chunk;
+    });
+
+    response.on("end", () => {
+      let object = JSON.parse(output);
+      result(response.statusCode, object);
+    });
+  });
+
+  request.on("error", (error) => {
+    console.error(error);
+  });
+
+  request.end();
+}
+
+module.exports = batchGetGooglePeople;
